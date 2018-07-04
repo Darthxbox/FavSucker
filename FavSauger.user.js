fa3934c9272c0dccb48b18617b6f0b9ec31e6cc3
Darthxbox <steve26@live.at>
2018-07-04 17:37:44 +0200

    Restrukturierung

--- FavSauger.user.js
+++ FavSauger.user.js
@@ -5,51 +5,48 @@
     // @description  Generiert Linkliste mit euren Favoriten
     // @author       Sector
     // @include      *pr0gramm.com/user/*/likes
-    // @icon		 http://pr0gramm.com/media/pr0gramm-favicon.png
-    // @grant		 unsafeWindow
-    // @run-at		 document-idle
+    // @icon         http://pr0gramm.com/media/pr0gramm-favicon.png
+    // @grant        unsafeWindow
+    // @run-at       document-idle
     // ==/UserScript==
+    
+    (function() {
 
-    (function()
-    {
+            var userHasScrolled = false;
+            unsafeWindow.onscroll = function(e) {
+                userHasScrolled = true;
+            }
 
-    	var userHasScrolled = false;
-    	unsafeWindow.onscroll = function (e)
-    	{
-    		userHasScrolled = true;
-    	}
+            var thumbs = unsafeWindow.document.getElementsByTagName('img')
+            var fav_array = unsafeWindow.document.getElementsByClassName("silent thumb");
+            var user_link = unsafeWindow.document.getElementById("user-profile-name").getAttribute("href");
+            var user = user_link.split("/")[2];
+            var bar = unsafeWindow.document.getElementById("head-menu")
+            var favDownload = "<a href='#' onclick='fetch()' class='user-only' id='upload-link' title='Favoriten downloaden'>Backup Favs</a>"
+            bar.innerHTML += favDownload;
+            unsafeWindow.download = function download(filename, text) {
+                var element = document.createElement('a');
+                element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+                element.setAttribute('download', filename);
+                element.style.display = 'none';
+                document.body.appendChild(element);
+                element.click();
+                document.body.removeChild(element);
+            }
+            unsafeWindow.fetch = function fetch() {
+                if (userHasScrolled) {
+                    var body = "";
+                    for (var i = 0; i < thumbs.length; i++) {
+                        body += thumbs[i].src.replace("thumb", "img") + "\n";
+                        body += thumbs[i].src.replace("thumb", "vid").replace(".jpg", ".mp4") + "\n";
+                        body += thumbs[i].src.replace("thumb", "img").replace(".jpg", ".gif") + "\n";
+                        body += thumbs[i].src.replace("thumb", "img").replace(".jpg", ".png") + "\n";
+                    }
+                    unsafeWindow.download(user + "Favorites.txt", body);
+                } else {
+                    alert("Du Schlingel hast nicht nach ganz unten gescrollt!");
+                }
+            }
+        }
 
-    	if(userHasScrolled)
-    	{
-    		var thumbs = unsafeWindow.document.getElementsByTagName('img')
-    		var fav_array = unsafeWindow.document.getElementsByClassName("silent thumb");
-    		var user_link = unsafeWindow.document.getElementById("user-profile-name").getAttribute("href");
-    		var user = user_link.split("/")[2];
-    		var bar = unsafeWindow.document.getElementById("head-menu")
-    		var favDownload = "<a href='#' onclick='fetch()' class='user-only' id='upload-link' title='Favoriten downloaden'>Backup Favs</a>"
-    		bar.innerHTML += favDownload;
-    		unsafeWindow.download = function download(filename, text) {
-    			var element = document.createElement('a');
-    			element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    			element.setAttribute('download', filename);
-    			element.style.display = 'none';
-    			document.body.appendChild(element);
-    			element.click();
-    			document.body.removeChild(element);
-    		}
-    		unsafeWindow.fetch = function fetch(){
-    			var body = "";
-    			for(var i = 0; i < thumbs.length; i++){
-    				body += thumbs[i].src.replace("thumb","img") + "\n";
-    				body += thumbs[i].src.replace("thumb","vid").replace(".jpg", ".mp4") + "\n";
-    				body += thumbs[i].src.replace("thumb","img").replace(".jpg", ".gif") + "\n";
-    				body += thumbs[i].src.replace("thumb","img").replace(".jpg", ".png") + "\n";
-    			}
-    			unsafeWindow.download(user + "Favorites.txt", body);
-    		}
-    	}
-    	else
-    	{
-    		alert("Du Schlingel hast nicht nach ganz unten gescrollt!");
-    	}
     }());
