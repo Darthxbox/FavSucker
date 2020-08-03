a1c015bea3b53915b50b8742d295fb845d9569a8
Sector <stephan.lorbek@aon.at>
2020-08-03 13:46:18 +0200

    Update FavSauger.user.js

--- FavSauger.user.js
+++ FavSauger.user.js
@@ -4,23 +4,24 @@
     // @version      1.0.1
     // @description  Generiert Linkliste mit euren Favoriten
     // @author       Sector
-    // @include      *pr0gramm.com/user/*/likes
+    // @include      *pr0gramm.com/user/*/favoriten
     // @icon         http://pr0gramm.com/media/pr0gramm-favicon.png
     // @grant        unsafeWindow
     // @run-at       document-idle
     // ==/UserScript==
-            var userHasScrolled = false;
+            unsafeWindow.userHasScrolled = false;
             unsafeWindow.onscroll = function(e) {
-                userHasScrolled = true;
+                 unsafeWindow.userHasScrolled = true;
             }
 
             var thumbs = unsafeWindow.document.getElementsByTagName('img')
             var fav_array = unsafeWindow.document.getElementsByClassName("silent thumb");
             var user_link = unsafeWindow.document.getElementById("user-profile-name").getAttribute("href");
             var user = user_link.split("/")[2];
-            var bar = unsafeWindow.document.getElementById("head-menu")
-            var favDownload = "<a href='#' onclick='fetch()' class='user-only' id='upload-link' title='Favoriten downloaden'>Backup Favs</a>"
+            var bar = unsafeWindow.document.getElementById("head-menu");
+            var favDownload = "<a onclick='getAll()' class='user-only' id='upload-link' title='Favoriten downloaden'>Backup Favs</a>";
             bar.innerHTML += favDownload;
+
             unsafeWindow.download = function download(filename, text) {
                 var element = document.createElement('a');
                 element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
@@ -29,9 +30,10 @@
                 document.body.appendChild(element);
                 element.click();
                 document.body.removeChild(element);
-            }
-            unsafeWindow.fetch = function fetch() {
-                if (userHasScrolled) {
+            };
+
+            unsafeWindow.getAll = function getAll() {
+                if (unsafeWindow.userHasScrolled == true) {
                     var body = "";
                     for (var i = 0; i < thumbs.length; i++) {
                         body += thumbs[i].src.replace("thumb", "img") + "\n";
@@ -43,4 +45,4 @@
                 } else {
                     alert("Du Schlingel hast nicht nach ganz unten gescrollt!");
                 }
-            }
+            };
